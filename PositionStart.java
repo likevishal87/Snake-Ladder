5426d6e010c0632abf83fb1aaaa46884f63145e1
Vicky Verma <likevishalverma@gmail.com>
2022-11-25 00:02:55 +0530

    Game played with single player

---
+++ PositionStart.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+//Snake and Ladder game played with single player at start position 0
+public class PositionStart {
+
+	public static void main(String[] args) {
+		final int POSITION = 0;
+
+		System.out.println("Let's begin the game...");
+		System.out.println("Initial position is Square Zero");
+		System.out.println("Position : Square " + POSITION);
+
+	}
+
+}
